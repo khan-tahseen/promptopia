9d429eee5481a9b3dd3263f0c06a63ccd0f91ca7
khan-tahseen <tasheenkhanit@gmail.com>
2024-01-16 23:06:20 +0530

    done with nav section

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -92,6 +92,25 @@ const Nav = () => {
                 >
                   My Profile
                 </Link>
+
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+
+                <button
+                  type="button"
+                  className='mt-4 w-full black_btn'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
               </div>
             )}
           </div>
