de913a200d26d5750c63375710943343691be160
khan-tahseen <tasheenkhanit@gmail.com>
2024-01-26 00:24:10 +0530

    working on user/profile =>

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,8 +3,21 @@ import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 
 import Profile from '@components/Profile';
+import { useEffect, useState } from 'react';
 
-const Profile = () => {
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
   const handleDelete = async () => {
     // Perform delete operation
     console.log('Deleting profile...');
@@ -19,11 +32,11 @@ const Profile = () => {
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={[]}
+      data={posts}
       handleDelete={handleDelete}
       handleEdit={handleEdit}
     />
   );
 };
 
-export default Profile;
+export default MyProfile;
